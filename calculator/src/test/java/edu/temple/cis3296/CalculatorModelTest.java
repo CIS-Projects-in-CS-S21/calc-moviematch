7f3b8dc9d6b5b2db81c6f77de6f7839b38f1ce42
Kyle Skelly <kyle.skelly32@gmail.com>
2021-03-01 21:15:31 -0500

    Added test for division

--- calculator/src/test/java/edu/temple/cis3296/CalculatorModelTest.java
+++ calculator/src/test/java/edu/temple/cis3296/CalculatorModelTest.java
@@ -34,7 +34,10 @@ public class CalculatorModelTest {
 		assertEquals(2.0, calc.operationDiv(2.0, 1.0), 0.1);
 	} 
 
-	
+	@Test
+	public void shouldDivTenByTwo(){
+		assertEquals(5.0, calc.operationDiv(10.0, 2.0), 0.1);
+	} 
 
 
 }
